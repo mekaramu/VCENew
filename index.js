15ac6d33642ec3c81badba204c2a7e5d74c1d90b
Rama Chandra Rao Meka <40292894+mekaramu@users.noreply.github.com>
2022-09-12 10:52:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'); //Import the express dependency
+const app = express();              //Instantiate an express app, the main work horse of this server
+const port = 5000;                  //Save the port number where your server will be listening
+
+//Idiomatic expression in express to route and respond to a client request
+app.get('/', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('login.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+});
+app.get('/login.js', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('login.js', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+});
+app.get('/style.css', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('style.css', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+});
+app.get('/success.html', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('success.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+});
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`); 
+});
